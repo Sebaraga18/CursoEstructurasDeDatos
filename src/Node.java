b7c00adc3ccdbd12ed6438e2dda9f3b862e01424
Sebastian Ramirez <seramirez3@poligran.edu.co>
2019-03-22 23:54:43 -0500

    Interface

---
+++ src/Node.java
@@ -0,0 +1,48 @@
+
+public interface Node {
+	
+	Node next = null; //Pointer
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String toString();
+	
+	
+	/**
+	 * 
+	 * @param node
+	 */
+	public void setNext(Node node);
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Node getNext();
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Node clone();
+	
+	
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public boolean isEqual(Node node);
+	
+	
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public boolean isLessThan(Node node);
+}
