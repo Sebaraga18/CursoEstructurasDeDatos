e8c8dffb672200eae729724cfc98d643e10e980c
Sebaraga18 <47919101+Sebaraga18@users.noreply.github.com>
2019-04-12 22:54:14 -0500

    Add files via upload

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -1,22 +1,6 @@
 package Trees;
 
 
-/*
-# Binary Tree Data Structure.
-#
-# Created by Msc. Carlos Andres Sierra on April 2018.
-# Copyright (c) 2018  Msc. Carlos Andres Sierra. Research Group on Artificial Life - ALIFE. All rights reserved.
-#
-# This file is part of DataStructuresTemplates.
-#
-# DataStructuresTemplates is free software: you can redistribute it and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation, version 3.
-*/
-
-/**
- * This class represents the behavior of Binary Trees
- * @author MSc. Carlos Andres Sierra, PhD. student
- */
 public class BinaryTree {
 	
 	BinaryTreeNode root = null; //
